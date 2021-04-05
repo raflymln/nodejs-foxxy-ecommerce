09f95e2046154f6fd56afec8d0a1fd1c2332558a
Rafly Maulana <mraflymaulana@gmail.com>
2021-04-05 17:14:56 +0700

    Update index.js

--- app/bot/index.js
+++ app/bot/index.js
@@ -1,58 +1,73 @@
 const Eris = require("eris");
-
 const bot = new Eris.CommandClient("NzkyMjQwMTM2ODYyNTY0MzYy.X-a1Hw.tWwuQVEK9jmNBZ3Bk4NMFMZTxCE", {}, {
-    description: "A test bot made with Eris",
-    owner: "somebody",
-    prefix: "fox"
+    prefix: ['fox', 'f!'],
+    defaultCommandOptions: {
+        requirements: {
+            userIDs: ['231721153444446208', '499021389572079620']
+        }
+    }
 });
 
-bot.on("ready", () => { // When the bot is ready
-    console.log("Ready!"); // Log "Ready!"
-});
-
-bot.registerCommand("blockip", (msg, args) => { // Make an echo command
+bot.registerCommand("blockip", async(msg, args) => {
     if (args.length === 0) return;
 
     for (const ip of args) {
-        const blockip = execute(`iptables -A INPUT -s ${ip} -j DROP`);
-        if (blockip) {
-            return `BLOCKED IP: ${ip}`
-        }
+        const blockip = await execute(`iptables -A INPUT -s ${ip} -j DROP`);
 
-        return `Failed to block ${ip}`
+        msg.channel.createMessage({
+            embed: {
+                description: (!!blockip) ? `Blocked IP: ${ip}` : `Failed to Block IP: ${ip}`,
+                color: 16498468
+            }
+        });
     }
 }, {
-    aliases: ['bip'],
-    requirements: {
-        userIDs: ['231721153444446208', '499021389572079620']
-    }
+    argsRequired: true,
+    aliases: ['bip', 'banip'],
 });
 
-const execute = (bash) => {
+bot.registerCommand("unblockip", async(msg, args) => {
+    if (args.length === 0) return;
+
+    for (const ip of args) {
+        const unblockip = await execute(`iptables -D INPUT -s ${ip} -j DROP`);
+
+        msg.channel.createMessage({
+            embed: {
+                description: (!!unblockip) ? `Unblocked IP: ${ip}` : `Failed to Unblock IP: ${ip}`,
+                color: 16498468
+            }
+        });
+    }
+}, {
+    argsRequired: true,
+    aliases: ['unbip', 'unbanip'],
+});
+
+bot.registerCommand("showblockedip", async(msg, args) => {
+    const iptables = await execute(`iptables -L INPUT -v -n`);
+
+    msg.channel.createMessage({
+        embed: {
+            description: "```" + iptables + "```",
+            color: 16498468
+        }
+    });
+}, {
+    aliases: ['sbip'],
+});
+
+const execute = async(bash) => {
     const { exec } = require("child_process");
 
     try {
-        console.log('============ BEGIN CHILD PROCCESS ============');
-        exec(bash, (error, stdout, stderr) => {
-            if (error) {
-                console.log(`error: ${error.message}`);
-                return;
-            }
-
-            if (stderr) {
-                console.log(`stderr: ${stderr}`);
-                return;
-            }
-
-            console.log(`stdout: ${stdout}`);
-            console.log('============ END CHILD PROCCESS ============')
-        });
-
-        return true;
+        const { stdout, stderr } = await exec(bash);
+        return { stdout, stderr };
     } catch (error) {
-        console.log(error)
+        console.log(error);
         return false;
     }
 }
 
+bot.on("ready", () => console.log("Ready!"));
 module.exports = bot.connect();
